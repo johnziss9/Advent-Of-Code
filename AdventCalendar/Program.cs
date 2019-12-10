bb2b153638cb63fdc85b5b16c063c8b5c9b594bc
Zissimou <ZissimouJ@b2b.regn.net>
2019-12-10 17:02:48 +0000

    Initial commit. - Solved.

--- AdventCalendar/Program.cs
+++ AdventCalendar/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace AdventCalendar
@@ -7,40 +8,52 @@ namespace AdventCalendar
     {
         static void Main(string[] args)
         {
-            string textFile = "Input.txt";
+            string line;
+            StreamReader file = new StreamReader(@"C:/Users/zissimouj/Desktop/ad.txt");
+            List<int> opcodes = new List<int>();
+            int currentPosition = 0;
+            int firstNumPosition = 0;
+            int secondNumPosition = 0;
+            int thirdNumPosition = 0;
 
-            string[] modules = File.ReadAllLines(textFile);
-            decimal totalFuel = 0;
-            decimal totalFuelOfFuel = 0;
-
-            foreach (var module in modules)
+            while ((line = file.ReadLine()) != null)
             {
-                decimal moduleFuel = 0;
-                decimal y = 0;
+                string[] codes = line.Split(',');
 
-                decimal division = Convert.ToInt32(module) / 3;
-                moduleFuel = Math.Floor(division) - 2;
-
-                totalFuel += moduleFuel;
-
-                y = totalFuel;
-
-                while (totalFuel > 0)
-                {
-                    decimal x = (Math.Floor(totalFuel / 3)) - 2;
-
-                    if (x < 0)
-                        x = 0;
-
-                    totalFuelOfFuel += x;
-                    totalFuel = x;
-                }
-
-                totalFuelOfFuel += y;
+                foreach (var code in codes)
+                    opcodes.Add(Convert.ToInt32(code));
             }
 
-            Console.WriteLine(totalFuelOfFuel);
-            Console.ReadLine();
+            opcodes[1] = 12;
+            opcodes[2] = 2;
+
+            while (currentPosition < opcodes.Count)
+            {
+                switch (opcodes[currentPosition])
+                {
+                    case 1:
+                        firstNumPosition = opcodes[currentPosition + 1];
+                        secondNumPosition = opcodes[currentPosition + 2];
+                        thirdNumPosition = opcodes[currentPosition + 3];
+                        opcodes[thirdNumPosition] = opcodes[firstNumPosition] + opcodes[secondNumPosition];
+                        currentPosition += 4;
+                        break;
+                    case 2:
+                        firstNumPosition = opcodes[currentPosition + 1];
+                        secondNumPosition = opcodes[currentPosition + 2];
+                        thirdNumPosition = opcodes[currentPosition + 3];
+                        opcodes[thirdNumPosition] = opcodes[firstNumPosition] * opcodes[secondNumPosition];
+                        currentPosition += 4;
+                        break;
+                    case 99:
+                        Console.WriteLine(opcodes[0]);
+                        Console.ReadLine();
+                        break;
+                    default:
+                        currentPosition++;
+                        break;
+                }
+            }
         }
     }
 }
