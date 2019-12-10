d8ac6aa7b7dee8cc77a2d9c05294dda1d7b6d12e
Zissimou <ZissimouJ@b2b.regn.net>
2019-12-10 16:53:00 +0000

    Initial commit. - Solved.

---
+++ AdventCalendar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace AdventCalendar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textFile = "Input.txt";
+
+            string[] modules = File.ReadAllLines(textFile);
+            decimal totalFuel = 0;
+
+            foreach (var module in modules)
+            {
+                decimal moduleFuel = 0;
+
+                decimal division = Convert.ToInt32(module) / 3;
+                moduleFuel = Math.Floor(division) - 2;
+
+                totalFuel += moduleFuel;
+            }
+
+            Console.WriteLine(totalFuel);
+            Console.ReadLine();
+        }
+    }
+}
