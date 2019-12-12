ab087789b144d8d3edc066419ead751a54e2bd15
John Zissimou <ZissimouJ@b2b.regn.net>
2019-12-12 10:03:39 +0000

    Initial commit. - Solved.

--- AdventCalendar/Program.cs
+++ AdventCalendar/Program.cs
@@ -11,47 +11,72 @@ namespace AdventCalendar
             string line;
             StreamReader file = new StreamReader(@"Input.txt");
             List<int> opcodes = new List<int>();
+            List<int> opcodesInitial = new List<int>();
+            bool halt = false;
             int currentPosition = 0;
             int firstNumPosition = 0;
             int secondNumPosition = 0;
             int thirdNumPosition = 0;
 
+            string[] codes = null;
+
             while ((line = file.ReadLine()) != null)
             {
-                string[] codes = line.Split(',');
+                codes = line.Split(',');
 
                 foreach (var code in codes)
+                {
                     opcodes.Add(Convert.ToInt32(code));
+                    opcodesInitial.Add(Convert.ToInt32(code));
+                }
             }
 
-            opcodes[1] = 12;
-            opcodes[2] = 2;
-
-            while (currentPosition < opcodes.Count)
+            for (int i = 0; i < 100; i++)
             {
-                switch (opcodes[currentPosition])
+                for (int j = 0; j < 100; j++)
                 {
-                    case 1:
-                        firstNumPosition = opcodes[currentPosition + 1];
-                        secondNumPosition = opcodes[currentPosition + 2];
-                        thirdNumPosition = opcodes[currentPosition + 3];
-                        opcodes[thirdNumPosition] = opcodes[firstNumPosition] + opcodes[secondNumPosition];
-                        currentPosition += 4;
-                        break;
-                    case 2:
-                        firstNumPosition = opcodes[currentPosition + 1];
-                        secondNumPosition = opcodes[currentPosition + 2];
-                        thirdNumPosition = opcodes[currentPosition + 3];
-                        opcodes[thirdNumPosition] = opcodes[firstNumPosition] * opcodes[secondNumPosition];
-                        currentPosition += 4;
-                        break;
-                    case 99:
-                        Console.WriteLine(opcodes[0]);
-                        Console.ReadLine();
-                        break;
-                    default:
-                        currentPosition++;
-                        break;
+                    // Clearing list and refilling it with original values
+                    opcodes.Clear();
+
+                    foreach (var code in codes)
+                    {
+                        opcodes.Add(Convert.ToInt32(code));
+                    }
+
+                    halt = false;
+
+                    opcodes[1] = i;
+                    opcodes[2] = j;
+
+                    while (halt == false && currentPosition < opcodes.Count)
+                    {
+                        switch (opcodes[currentPosition])
+                        {
+                            case 1:
+                                firstNumPosition = opcodes[currentPosition + 1];
+                                secondNumPosition = opcodes[currentPosition + 2];
+                                thirdNumPosition = opcodes[currentPosition + 3];
+                                opcodes[thirdNumPosition] = opcodes[firstNumPosition] + opcodes[secondNumPosition];
+                                currentPosition += 4;
+                                break;
+                            case 2:
+                                firstNumPosition = opcodes[currentPosition + 1];
+                                secondNumPosition = opcodes[currentPosition + 2];
+                                thirdNumPosition = opcodes[currentPosition + 3];
+                                opcodes[thirdNumPosition] = opcodes[firstNumPosition] * opcodes[secondNumPosition];
+                                currentPosition += 4;
+                                break;
+                            case 99:
+                                if (opcodes[0] == 19690720)
+                                    Console.WriteLine(100 * i + j);
+                                halt = true;
+                                currentPosition = 0;
+                                break;
+                            default:
+                                currentPosition++;
+                                break;
+                        }
+                    }
                 }
             }
         }
