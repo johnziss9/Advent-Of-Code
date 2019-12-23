4a59a0f8c7e2cb0ffd3650b72c7ad4046f59cdc9
John Zissimou <ZissimouJ@b2b.regn.net>
2019-12-23 12:41:38 +0000

    Initial commit of Day3Part2 - Solved.

--- AdventCalendar/Program.cs
+++ AdventCalendar/Program.cs
@@ -14,10 +14,13 @@ namespace AdventCalendar
             string[] directionsSplit = null;
             int cable1Horizontal = 0;
             int cable1Vertical = 0;
-            List<string> positions = new List<string>();
-            List<string> intersections = new List<string>();
-            List<int> totals = new List<int>();
-            
+            int cable1StepCount = 0;
+            int cable2StepCount = 0;
+            List<string> positions = new List<string>(); // Records each position of the first cable after every move
+            List<string> intersections = new List<string>(); // Stores all the intersections cable 2 hits on cable plus adds the numbers of moves both cable took to get there
+            List<int> totalSteps = new List<int>(); // Stores total amount of steps taken for each intersection for both cables (taken from 'intersections' list)
+            List<int> cable1Steps = new List<int>(); // Stores the step count after each move cable 1 makes
+
             StreamReader file = new StreamReader(@"Input.txt");
 
             while ((line = file.ReadLine()) != null)
@@ -34,22 +37,38 @@ namespace AdventCalendar
                         {
                             case 'R':
                                 for (int i = 1; i <= Convert.ToInt32(direction.Substring(1)); i++)
+                                {
+                                    cable1StepCount++;
                                     positions.Add(cable1Horizontal + i + ":" + cable1Vertical);
+                                    cable1Steps.Add(cable1StepCount);
+                                }                                    
                                 cable1Horizontal += Convert.ToInt32(direction.Substring(1));
                                 break;
                             case 'U':
                                 for (int i = 1; i <= Convert.ToInt32(direction.Substring(1)); i++)
+                                {
+                                    cable1StepCount++;
                                     positions.Add(cable1Horizontal + ":" + (cable1Vertical + i));
+                                    cable1Steps.Add(cable1StepCount);
+                                }                                    
                                 cable1Vertical += Convert.ToInt32(direction.Substring(1));
                                 break;
                             case 'L':
                                 for (int i = 1; i <= Convert.ToInt32(direction.Substring(1)); i++)
+                                {
+                                    cable1StepCount++;
                                     positions.Add((cable1Horizontal - i) + ":" + cable1Vertical);
+                                    cable1Steps.Add(cable1StepCount);
+                                }
                                 cable1Horizontal -= Convert.ToInt32(direction.Substring(1));
                                 break;
                             case 'D':
                                 for (int i = 1; i <= Convert.ToInt32(direction.Substring(1)); i++)
+                                {
+                                    cable1StepCount++;
                                     positions.Add(cable1Horizontal + ":" + (cable1Vertical - i));
+                                    cable1Steps.Add(cable1StepCount);
+                                }
                                 cable1Vertical -= Convert.ToInt32(direction.Substring(1));
                                 break;
                             default:
@@ -76,32 +95,53 @@ namespace AdventCalendar
                             case 'R':
                                 for (int i = 1; i <= Convert.ToInt32(direction.Substring(1)); i++)
                                 {
+                                    cable2StepCount++;
+
                                     if (positions.Contains((cable1Horizontal + i) + ":" + cable1Vertical))
-                                        intersections.Add((cable1Horizontal + i) + ":" + cable1Vertical);
+                                    {
+                                        var cable1Index = positions.IndexOf((cable1Horizontal + i) + ":" + cable1Vertical);
+                                        intersections.Add((cable1Horizontal + i) + ":" + cable1Vertical + ":" + (cable2StepCount + Convert.ToInt32(cable1Steps[cable1Index])));
+                                    }
+                                        
                                 }
                                 cable1Horizontal += Convert.ToInt32(direction.Substring(1));
                                 break;
                             case 'U':
                                 for (int i = 1; i <= Convert.ToInt32(direction.Substring(1)); i++)
                                 {
+                                    cable2StepCount++;
+
                                     if (positions.Contains(cable1Horizontal + ":" + (cable1Vertical + i)))
-                                        intersections.Add(cable1Horizontal + ":" + (cable1Vertical + i));
+                                    {
+                                        var cable1Index = positions.IndexOf(cable1Horizontal + ":" + (cable1Vertical + i));
+                                        intersections.Add(cable1Horizontal + ":" + (cable1Vertical + i) + ":" + (cable2StepCount + Convert.ToInt32(cable1Steps[cable1Index])));
+                                    }                                        
                                 }
                                 cable1Vertical += Convert.ToInt32(direction.Substring(1));
                                 break;
                             case 'L':
                                 for (int i = 1; i <= Convert.ToInt32(direction.Substring(1)); i++)
                                 {
+                                    cable2StepCount++;
+
                                     if (positions.Contains((cable1Horizontal - i) + ":" + cable1Vertical))
-                                        intersections.Add((cable1Horizontal - i) + ":" + cable1Vertical);
+                                    {
+                                        var cable1Index = positions.IndexOf((cable1Horizontal - i) + ":" + cable1Vertical);
+                                        intersections.Add((cable1Horizontal - i) + ":" + cable1Vertical + ":" + (cable2StepCount + Convert.ToInt32(cable1Steps[cable1Index])));
+                                    }                                        
                                 }
                                 cable1Horizontal -= Convert.ToInt32(direction.Substring(1));
                                 break;
                             case 'D':
                                 for (int i = 1; i <= Convert.ToInt32(direction.Substring(1)); i++)
                                 {
+                                    cable2StepCount++;
+
                                     if (positions.Contains(cable1Horizontal + ":" + (cable1Vertical - i)))
-                                        intersections.Add(cable1Horizontal + ":" + (cable1Vertical - i));
+                                    {
+                                        var cable1Index = positions.IndexOf(cable1Horizontal + ":" + (cable1Vertical - i));
+                                        intersections.Add(cable1Horizontal + ":" + (cable1Vertical - i) + ":" + (cable2StepCount + Convert.ToInt32(cable1Steps[cable1Index])));
+                                    }                                        
                                 }
                                 cable1Vertical -= Convert.ToInt32(direction.Substring(1));
                                 break;
@@ -116,14 +156,14 @@ namespace AdventCalendar
                         string[] items = intersection.Split(":");
                         var result = Convert.ToInt32(items[0]) + Convert.ToInt32(items[1]);
 
-                        // Distance need to be above 0
-                        if (result > 0)
-                            totals.Add(result);
+                        // Distance needs to be above 0
+                        // if (result > 0)
+                            totalSteps.Add(Convert.ToInt32(items[2]));
                     }
 
-                    // Get lowest distance
-                    int lowestDistance = totals.Min();
-                    Console.WriteLine(lowestDistance);
+                    // Get lowest step count
+                    int lowstStepCount = totalSteps.Min();
+                    Console.WriteLine(lowstStepCount);
 
                     Console.ReadLine();
                 }
